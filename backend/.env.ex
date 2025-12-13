8ca7fe09163d098ebd41b46e30379be536cb227f
Tejas-sonar7 <tejasmore101204@gmail.com>
2025-12-13 15:03:20 +0530

    Reapply "tejas"
    This reverts commit 4f70ff99b2b3f7c3de066c8722a61532e8207e35.

---
+++ backend/.env.ex
@@ -0,0 +1,7 @@
+PORT=5000
+MONGO_URI=mongodb://localhost:27017/shivohini
+EMAIL_USER=ni*********@gmail.com
+EMAIL_PASS=**************
+HR_EMAIL=******************@gmail.com
+HR_PASS=**************
+
