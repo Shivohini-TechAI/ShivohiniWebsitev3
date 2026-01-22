f8e5befe8887f5ed3d1084c5259c84a7042b11e4
prasanna4149 <150417523+prasanna4149@users.noreply.github.com>
2026-01-22 10:21:10 +0530

    backend

--- backend/.env.ex
+++
@@ -1,7 +0,0 @@
-PORT=5000
-MONGO_URI=mongodb://localhost:27017/shivohini
-EMAIL_USER=ni*********@gmail.com
-EMAIL_PASS=**************
-HR_EMAIL=******************@gmail.com
-HR_PASS=**************
-
